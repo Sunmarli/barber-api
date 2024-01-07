f71a749facc89845aad9783c61dbae3b99d7c9fc
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2024-01-07 17:52:46 +0200

    test

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port=8081
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/barber', (req,res)=>{
+    res.send(["John Wick", "Will Smith"])
+})
+
+
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
