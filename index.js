cfd5ccf3580bea3ea9247b45caf67c8842fb675b
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2024-01-07 12:41:02 +0200

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+const app=require('express')()
+const port=8081
+
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+
+app.listen(port,()=>{
+    console.log(`API up at: http://localhost:${port}`)
+})
