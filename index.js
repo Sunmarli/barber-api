759c58e34d5cd88b8a9a66d77cd927accdb11fc2
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2024-01-07 19:28:31 +0200

    23

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const app=require('express')()
 const port=8081
 
-const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.json');
+const swaggerUI = require('swagger-ui-express')
+const yamljs=require('yamljs')
+const swaggerDocument=yamljs.load('./docs/swagger.yaml')
 
-app.get('/barber', (req,res)=>{
-    res.send(["John Wick", "Will Smith"])
+//const swaggerDocument = require('./docs/swagger.json');
+
+
+const barbers = [
+    { id: 1, name: "John Wick", working_day: "Monday", specialization: "Haircuts" },
+    { id: 2, name: "Will Smith", working_day: "Tuesday", specialization: "Beard Trims" },
+    { id: 3, name: "Siim Tamm", working_day: "Wednesday", specialization: "Coloring" },
+    { id: 4, name: "Marko Polo", working_day: "Thursday", specialization: "Haircuts" },
+    { id: 5, name: "Basim Muhha", working_day: "Friday", specialization: "Hair Styling" },
+    { id: 6, name: "Kerli Ostrov", working_day: "Saturday", specialization: "Facial Treatments" },
+    { id: 7, name: "Siim Kallas", working_day: "Sunday", specialization: "Haircuts" }
+];
+
+
+app.get('/barbers', (req,res)=>{
+    res.send(barbers)
 })
+app.get('/barbers/:id', (req,res)=>{
+
+    if (typeof barbers[req.params.id -1]==='undefined'){
+        return res.status(404).send({error:"Barber not found"})
+    }
+    res.send(barbers[req.params.id -1])    
+})
+
+app.post('/barbers',(req,res)=>{
+    barbers.push({
+        id:barbers.length +1,
+        name:req.body.name,
+        working_day:req.body.working_day,
+        specialization:req.body.specialization
+    })
+    res.end()
+})
+
+
 
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
